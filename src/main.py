4e9da35df68294552be01e5b4bb0ddb7e574e8cd
Ylja07 <yson@avans.nl>
2020-12-06 23:25:41 +0100

    Small fixes. pruning.py return dict of best params. main.py:  added extra replace row in prep_data()  Fixed random_forest()  Added kwargs for easy usability  Main() is now a loop to test the forest functions.

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ from pruning import Pruning
 # load data
 dataset = pd.read_csv("../data/student-por.csv", sep=";")
 
+holdout_data = []
+
 
 def prep_data():
     """
@@ -37,17 +39,36 @@ def prep_data():
     dataset['higher'].replace({'no': 0, 'yes': 1}, inplace=True)
     dataset['internet'].replace({'no': 0, 'yes': 1}, inplace=True)
     dataset['romantic'].replace({'no': 0, 'yes': 1}, inplace=True)
+    # TODO: change G3 to binary passed or not (need to verify)
+    dataset['G3'].replace({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0,
+                           10: 1, 11: 1, 12: 1, 13: 1, 14: 1, 15: 1, 16: 1, 17: 1, 18: 1, 19: 1, 20: 1}, inplace=True)
 
 
 def random_forest(x_placeholder, y_placeholder, **kwarg):
-    clf = RandomForestClassifier(kwargs)
+    clf = RandomForestClassifier(**kwarg)
     clf.fit(x_placeholder, y_placeholder)
+    return clf
+
+def split_data(x_data, y_data, test_size):
+    X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=test_size)
+    return X_train, X_test, y_train, y_test
 
 
 def main():
+    # Prep data for usage.
     prep_data()
     X = dataset.loc[:, dataset.columns != 'G3']
     y = dataset['G3']
-    Pruning.random_forest_hyper_parameters(X, y)
+    X_train, X_test, y_train, y_test = split_data(X, y, 0.2)
+
+    # Tune the hyper parameters & fit the forrest.
+    kargs = Pruning.random_forest_hyper_parameters(X_train, y_train)
+    forest = random_forest(X_train, y_train, **kargs)
+
+    # Get accuracy
+    predict_list = forest.predict(X_test)
+    accuracy = accuracy_score(y_test, predict_list)
+    print(accuracy)
+
 
 main()
